0748ae057f942ffa927e2136355c299da2dd0b79
ayaauuu <ayauabai05@gmail.com>
2026-06-13 11:31:56 +0300

    Create short_hand_if_else.py

---
+++ if_else/short_hand_if_else.py
@@ -0,0 +1,14 @@
+a = 10
+b = 20
+
+print("a is greater") if a > b else print("b is greater")
+
+x = 5
+y = 5
+
+print("Equal") if x == y else print("Not Equal")
+
+age = 19
+
+result = "Adult" if age >= 18 else "Minor"
+print(result)
