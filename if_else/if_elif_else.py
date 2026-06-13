2f5aa863b3c4f9bd90383cec6617b78b4c263a59
ayaauuu <ayauabai05@gmail.com>
2026-06-13 11:30:52 +0300

    Create if_elif_else.py

---
+++ if_else/if_elif_else.py
@@ -0,0 +1,19 @@
+score = 85
+
+if score >= 90:
+    print("Grade A")
+elif score >= 75:
+    print("Grade B")
+elif score >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+temperature = 0
+
+if temperature > 0:
+    print("Above freezing")
+elif temperature == 0:
+    print("Freezing point")
+else:
+    print("Below freezing")
