4823d354d2f1410464a4032e5976ed9e80ec934e
ayaauuu <ayauabai05@gmail.com>
2026-06-13 11:29:57 +0300

    Create if_else.py

---
+++ if_else/if_else.py
@@ -0,0 +1,20 @@
+age = 16
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+number = -5
+
+if number > 0:
+    print("Positive")
+else:
+    print("Negative")
+
+marks = 40
+
+if marks >= 50:
+    print("Pass")
+else:
+    print("Fail")
