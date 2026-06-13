c9691bb5b08c28894ab23fa73fa871492b7ea7fa
ayaauuu <ayauabai05@gmail.com>
2026-06-13 11:37:13 +0300

    Create for_continue.py

---
+++ loops/for_continue.py
@@ -0,0 +1,15 @@
+for i in range(10):
+
+    if i == 5:
+        continue
+
+    print(i)
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+
+    if fruit == "banana":
+        continue
+
+    print(fruit)
