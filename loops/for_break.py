ae3193a00adaf8193be717bdc1db96ae164778aa
ayaauuu <ayauabai05@gmail.com>
2026-06-13 11:36:22 +0300

    Create for_break.py

---
+++ loops/for_break.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "banana", "cherry", "orange"]
+
+for fruit in fruits:
+    print(fruit)
+
+    if fruit == "cherry":
+        break
+
+for i in range(10):
+    if i == 6:
+        break
+
+    print(i)
