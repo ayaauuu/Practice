b7f401ffbfede098360cb2093799a27318f71047
ayaauuu <ayauabai05@gmail.com>
2026-06-20 01:53:39 +0300

    Create init_method.py

---
+++ Practice3/classes/init_method.py
@@ -0,0 +1,11 @@
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+student1 = Student("Aya", 19)
+student2 = Student("Ali", 20)
+
+print(student1.name)
+print(student2.age)
