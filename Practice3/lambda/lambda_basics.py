16c1224f7549ccae04fa98268708bf5fa824233a
ayaauuu <ayauabai05@gmail.com>
2026-06-20 01:50:21 +0300

    Create lambda_basics.py

---
+++ Practice3/lambda/lambda_basics.py
@@ -0,0 +1,15 @@
+#1
+square = lambda x: x * x
+print(square(5))
+
+#2
+add = lambda a, b: a + b
+print(add(10, 20))
+
+#3
+multiply = lambda x, y: x * y
+print(multiply(3, 7))
+
+#4
+uppercase = lambda text: text.upper()
+print(uppercase("python"))
