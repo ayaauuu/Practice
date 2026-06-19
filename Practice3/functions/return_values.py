7a91af18a5e5c367b366456f8d131185217cf1d8
ayaauuu <ayauabai05@gmail.com>
2026-06-20 01:48:27 +0300

    Add return_values.py with multiple functions

---
+++ Practice3/functions/return_values.py
@@ -0,0 +1,23 @@
+#1
+def add(a, b):
+    return a + b
+
+print(add(3, 7))
+
+#2
+def square(x):
+    return x * x
+
+print(square(6))
+
+#3
+def is_even(number):
+    return number % 2 == 0
+
+print(is_even(8))
+
+#4
+def full_name(first, last):
+    return first + " " + last
+
+print(full_name("Ayau", "Abai"))
