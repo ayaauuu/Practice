ce496ffbf21c0bd11e5747ba36d8453c8253cfff
ayaauuu <ayauabai05@gmail.com>
2026-06-20 01:45:24 +0300

    Create basic_functions.py

---
+++ Practice3/functions/basic_functions.py
@@ -0,0 +1,23 @@
+#1
+def greet():
+    print("Hello!")
+
+greet()
+
+#2
+def welcome(name):
+    print("Welcome,", name)
+
+welcome("Aya")
+
+#3
+def multiply(a, b):
+    print(a * b)
+
+multiply(5, 4)
+
+#4
+def course():
+    print("Python Programming")
+
+course()
